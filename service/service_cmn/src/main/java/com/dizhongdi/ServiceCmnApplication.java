3b72264148f3944076b4e54f7ad96e7c0f511c09
dizhongdi <2755063993@qq.com>
2022-02-14 23:24:23 +0800

    添加nacos

--- service/service_cmn/src/main/java/com/dizhongdi/ServiceCmnApplication.java
+++ service/service_cmn/src/main/java/com/dizhongdi/ServiceCmnApplication.java
@@ -2,10 +2,12 @@ package com.dizhongdi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServiceCmnApplication {
 
     public static void main(String[] args) {
--- service/service_hosp/src/main/java/com/dizhongdi/yygh/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/dizhongdi/yygh/ServiceHospApplication.java
@@ -3,11 +3,13 @@ package com.dizhongdi.yygh;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.dizhongdi")
+@EnableDiscoveryClient
 public class ServiceHospApplication {
 
     public static void main(String[] args) {
